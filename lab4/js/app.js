eb8820cc393b436a5cceaa226886e861163068e5
kinoodaphne <daphne.kinoo@outlook.be>
2019-03-29 14:25:26 +0100

    Create base for the weather script
    Created base for the script.
    This will get the current weather and display it on our body.

--- lab4/js/app.js
+++ lab4/js/app.js
@@ -1,30 +1,38 @@
-class Weather
-{
-    constructor(options){
-        
+class Weather {
+    constructor(API_KEY) {
+        this.API_KEY = API_KEY;
+        console.log("🚀👀");
+        this.initialize();
     }
 
-    getCurrentLocation(){
-        
-    }    
-
-    getWeather(position){
-        
+    initialize() {
+        this.getMyLocation();
     }
 
-    updateUI(){
-        
+    getMyLocation() {
+        console.log("Getting location ☀️");
+        navigator.geolocation.getCurrentPosition(position => {
+            console.log("found you");
+            let lat = position.coords.latitude;
+            let lng = position.coords.longitude;
+            console.log(position);
+            this.getWeather(lat, lng);
+        }, err =>{
+            console.log(err)
+        });
     }
 
-    init(){
-        
+    getWeather(lat, lng) {
+        //AJAX CALL / XHR
+        let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${this.API_KEY}/${lat},${lng}`;
+        fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            console.log(json.currently.summary);
+        });
     }
-
 }
 
-
-const options = {
-    'apikey': '',
-    'el': '#app'
-}
-const App = new Weather(options);
+let app = new Weather('99fc788f5b715f9a75262427d4da53aa');
