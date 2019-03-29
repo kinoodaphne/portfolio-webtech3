9f7ad8da66ba0d7d4fd7b134ac031df6f076883d
kinoodaphne <daphne.kinoo@outlook.be>
2019-03-29 14:40:07 +0100

    Created getImage - app.js
    Created getImage for the second API key

--- lab4/js/app.js
+++ lab4/js/app.js
@@ -1,6 +1,7 @@
 class Weather {
-    constructor(API_KEY) {
+    constructor(API_KEY, API_KEY_IMG) {
         this.API_KEY = API_KEY;
+        this.API_KEY_IMG = API_KEY_IMG;
         console.log("🚀👀");
         this.initialize();
     }
@@ -15,6 +16,7 @@ class Weather {
             console.log("found you");
             let lat = position.coords.latitude;
             let lng = position.coords.longitude;
+            console.log("This is your current position");
             console.log(position);
             this.getWeather(lat, lng);
         }, err =>{
@@ -23,7 +25,7 @@ class Weather {
     }
 
     getWeather(lat, lng) {
-        //AJAX CALL / XHR
+        //API_KEY for weather
         let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${this.API_KEY}/${lat},${lng}`;
         fetch(url)
         .then(response => {
@@ -31,8 +33,17 @@ class Weather {
         })
         .then(json => {
             console.log(json.currently.summary);
+            let img = json.currently.icon;
+            this.getImage(img);
+            let app = new Image('e59jizvvHYSZ3rKBBk0xLWmE4ElKZ4on');
         });
     }
-}
 
-let app = new Weather('99fc788f5b715f9a75262427d4da53aa');
+    getImage(img) {
+        //API_KEY for image
+        let url = ``;
+    }
+
+}
+//let app = new Weather(API_KEY, API_KEY_IMG);
+let app = new Weather('99fc788f5b715f9a75262427d4da53aa', 'e59jizvvHYSZ3rKBBk0xLWmE4ElKZ4on');
