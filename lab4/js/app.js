8c8436b764201b477f10da3f2add8a3f33dcbb4d
kinoodaphne <daphne.kinoo@outlook.be>
2019-03-31 17:13:27 +0200

    Edited everything
    - Deleted everything from the HTML.
    - Fixed app.js script so it works, load HTML content in Javascript
    - Updates CSS file

--- lab4/js/app.js
+++ lab4/js/app.js
@@ -1,61 +1,54 @@
-class Weather {
-    constructor(API_KEY, API_KEY_IMG) {
+class Weather{
+    constructor(API_KEY){
         this.API_KEY = API_KEY;
-        this.API_KEY_IMG = API_KEY_IMG;
-        console.log("🚀👀");
         this.initialize();
-    }
+    } 
+ 
+ initialize() {
+     this.getMyLocation();
+     this.getFact();
+ }
+ getMyLocation(){
+   //console.log("get location"); 
+   navigator.geolocation.getCurrentPosition(position => {
+     //console.log (position);
+     let lat = position.coords.latitude;
+     let long = position.coords.longitude;
+ 
+     this.getWeather(lat, long);
+   }, err => {
+     console.log(err);
+   });
+ }
+ getWeather(lat, long){
+     let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${this.API_KEY}/${lat},${long}?units=si`;
+     fetch(url)
+     .then(response =>{
+         return response.json();
+     })
+     .then(json =>{
+         let temp = document.createElement("h1");
+         temp.innerHTML = `Today is ${json.currently.summary}`;
+         document.querySelector("body").appendChild(temp);
+     });
+ }
+ 
+ getFact(){
 
-    initialize() {
-        this.getMyLocation();
-    }
-
-    getMyLocation() {
-        console.log("Getting location ☀️");
-        navigator.geolocation.getCurrentPosition(position => {
-            console.log("found you");
-            let lat = position.coords.latitude;
-            let lng = position.coords.longitude;
-            console.log("This is your current position");
-            console.log(position);
-            this.getWeather(lat, lng);
-        }, err =>{
-            console.log(err)
-        });
-    }
-
-    getWeather(lat, lng) {
-        //API_KEY for weather
-        let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${this.API_KEY}/${lat},${lng}`;
+    let url = `https://cors-anywhere.herokuapp.com/https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1`;
         fetch(url)
-        .then(response => {
+        
+        .then(response =>{
             return response.json();
+            console.log(json);
         })
-        .then(json => {
-            console.log(json.currently.summary);
-            let img = json.currently.icon;
-            this.getImage(img);
-            let app = new Image('e59jizvvHYSZ3rKBBk0xLWmE4ElKZ4on');
+        .then(json =>{
+            console.log(json);
+            console.log(json.text);
+            document.querySelector("body").innerHTML += `<p>${json.text}</p>`;
         });
-    }
-
-    getImage(img) {
-        //API_KEY for image
-        let url = `https://cors-anywhere.herokuapp.com/https://api.giphy.com/v1/gifs/random?${this.API_KEY_IMG}}`;
-        fetch(url)
-        .then(response => {
-            return response.json();
-        })
-        .then(json => {
-            // generate random number for the api
-            let random = Math.floor(Math.random() * 10);
-            let imgRandom = json.htis[random].largeImageUrl;
-            document.querySelector(".image").setAttribute("src", imgRandom);
-            
-            document.querySelector(".temp").innerHTML = ("Today is " + picture);
-
-        })
-    }
-
-//let app = new Weather(API_KEY, API_KEY_IMG);
-let app = new Weather('99fc788f5b715f9a75262427d4da53aa');
+ } 
+  
+ }
+ 
+ let app = new Weather('99fc788f5b715f9a75262427d4da53aa');
